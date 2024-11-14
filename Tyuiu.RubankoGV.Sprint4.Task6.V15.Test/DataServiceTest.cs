ed593b7e731d0a1266289c10d4c038a531dddccd
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-14 20:11:40 +0300

    Task6

---
+++ Tyuiu.RubankoGV.Sprint4.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.RubankoGV.Sprint4.Task6.V15.Lib;
+namespace Tyuiu.RubankoGV.Sprint4.Task6.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.RubankoGV.Sprint4.Task6.V15/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.RubankoGV.Sprint4.Task6.V15.Lib;
+namespace Tyuiu.RubankoGV.Sprint4.Task6.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            
+            Console.WriteLine("* УСЛОВИЕ:                                                   *");
+            Console.WriteLine("* Дан строковый массив данных [Чикаго, Хьюстон, Феникс,      *");
+            Console.WriteLine("* Филадельфия,Сан-Антонио,Сан-Диего,Даллас] используя класс  *");
+            Console.WriteLine("* Array подсчитайте кол-во элементов, длина которых меньше 7 *");
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                           *");
+            Console.WriteLine("**************************************************************");
+
+            var week = new string[] { "Чикаго", "Хьюстон", "Феникс", "Филадельфия", "Сан-Антонио", "Сан-Диего", "Даллас" };
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i <= week.Length - 1; i++)
+            {
+
+                Console.WriteLine(week[i]);
+            }
+
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                 *");
+            Console.WriteLine("**************************************************************");
+
+            Console.WriteLine("количество элементов длина которых < 7: ");
+            int nums = ds.Calculate(week);
+            Console.WriteLine(nums);
+            Console.ReadKey();
+        }
+    }
+}
