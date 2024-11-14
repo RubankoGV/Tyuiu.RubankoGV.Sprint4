4c8c95697cae2ea00e76e2b9984c15e0ab425d58
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-14 19:42:13 +0300

    Task3

---
+++ Tyuiu.RubankoGV.Sprint4.Task3.V3.Test/DataServicetest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.RubankoGV.Sprint4.Task3.V3.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Tyuiu.RubankoGV.Sprint4.Task3.V3/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.RubankoGV.Sprint4.Task3.V3.Lib;
+namespace Tyuiu.RubankoGV.Sprint4.Task3.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 2 до 9. Найдите сумму элементов  *");
+            Console.WriteLine("* в первом столбце массива.                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            Console.WriteLine("Массив:");
+            int[,] array = new int[,] { { 3, 4, 8, 6, 3 }, { 9, 9, 3, 6, 9 }, { 5, 6, 7, 2, 3 }, { 3, 6, 2, 3, 8 }, { 5, 7, 2, 6, 4 } };
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(array);
+            Console.WriteLine("Сумма элементов в первом стобце массива: " + res);
+            Console.ReadKey();
+        }
+    }
+}
